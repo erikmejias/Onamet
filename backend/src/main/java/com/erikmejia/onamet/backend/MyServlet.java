9435a643891f622748ea8e32b6b8c1f7425f07ed
Erik Mejia <erik@xlinfe.com>
2016-10-13 14:08:49 -0400

    Connecting plumbing to Firebase Database

--- backend/src/main/java/com/erikmejia/onamet/backend/MyServlet.java
+++ backend/src/main/java/com/erikmejia/onamet/backend/MyServlet.java
@@ -6,26 +6,94 @@
 
 package com.erikmejia.onamet.backend;
 
-import java.io.IOException;
+import com.google.api.client.googleapis.auth.clientlogin.ClientLogin;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.*;
+import javax.xml.ws.Response;
 
 public class MyServlet extends HttpServlet {
+    static Logger Log = Logger.getLogger("com.erikmejia.onamet.backend.MyServlet");
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Please use the form to POST to this url");
-    }
+        Log.info("Refreshing Firebase Database");
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        String name = req.getParameter("name");
-        resp.setContentType("text/plain");
-        if (name == null) {
-            resp.getWriter().println("Please enter a name");
+        String outString;
+        outString = "<p>Sending message from App Engine to Firebase";
+
+        resp.getWriter().println(outString);
+
+        // Note: Ensure that the [PRIVATE_KEY_FILENAME].json has read
+        // permissions set.
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setServiceAccount(getServletContext().getResourceAsStream("/WEB-INF/Onamet-035fa87b100b.json"))
+                .setDatabaseUrl("https://project-7000350159161293832.firebaseio.com/")
+                .build();
+
+        try {
+            FirebaseApp.getInstance();
         }
-        resp.getWriter().println("Hello " + name);
+        catch (Exception error){
+            Log.info("doesn't exist...");
+        }
+
+        try {
+            FirebaseApp.initializeApp(options);
+        }
+        catch(Exception error){
+            Log.info("already exists...");
+        }
+
+        // As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference("forecasts/azua");
+
+        // This fires when the servlet first runs, returning all the existing values
+        // only runs once, until the servlet starts up again.
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                Log.info("new value: "+ document);
+
+                String todoText = "Don't forget to...\n\n";
+
+                Iterator<DataSnapshot> children = dataSnapshot.getChildren().iterator();
+
+                while(children.hasNext()){
+                    DataSnapshot childSnapshot = (DataSnapshot) children.next();
+                    todoText = todoText + " * " + childSnapshot.getValue().toString() + "\n";
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error){
+                System.out.println("Error: "+error);
+            }
+        });
     }
 }
