214003c09924faccff3986eee5e50604bae6a5af
Erik Mejia <erik@xlinfe.com>
2016-10-25 23:00:47 -0400

    Refined UI of Onboarding experience

--- app/src/main/java/com/erikmejia/onamet/IntroActivity.java
+++ app/src/main/java/com/erikmejia/onamet/IntroActivity.java
@@ -1,14 +1,9 @@
 package com.erikmejia.onamet;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 
-import com.erikmejia.onamet.ui.IntroFragmentFour;
-import com.erikmejia.onamet.ui.IntroFragmentOne;
-import com.erikmejia.onamet.ui.IntroFragmentThree;
-import com.erikmejia.onamet.ui.IntroFragmentTwo;
+import com.erikmejia.onamet.ui.SampleSlide;
 import com.github.paolorotolo.appintro.AppIntro2;
-import com.github.paolorotolo.appintro.AppIntro2Fragment;
 
 public class IntroActivity extends AppIntro2 {
 
@@ -16,42 +11,15 @@ public class IntroActivity extends AppIntro2 {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        setWizardMode(true);
 
-//        Custom fragments
-        IntroFragmentOne fragmentOne = new IntroFragmentOne();
-        IntroFragmentTwo fragmentTwo = new IntroFragmentTwo();
-        IntroFragmentThree fragmentThree = new IntroFragmentThree();
-        IntroFragmentFour fragmentFour = new IntroFragmentFour();
-
-        addSlide(AppIntro2Fragment.newInstance("Bienvenido a Onamet",
-                "/fonts/Brandon_light.otf",
-                "Podras estar al tanto de toda información relacionada con el clima en República Dominicana",
-                "/fonts/Brandon_reg.otf",
-                R.drawable.weather_clear, getResources().getColor(R.color.colorPrimary)));
-
-        addSlide(AppIntro2Fragment.newInstance("Pronósticos diarios",
-                "fonts/Brandon_thin.otf",
-                "Recibes estado del clima para los próximos 13 días",
-                "fonts/Brandon_reg.otf",
-                R.drawable.weather_few_clouds, getResources().getColor(R.color.colorPrimaryDark)));
-
-        addSlide(AppIntro2Fragment.newInstance("Boletines Oficiales",
-                "fonts/Brandon_light.otf",
-                "Los boletines oficiales al instante, para que estés al tanto de lo último",
-                "fonts/Brandon_light.otf",
-                R.drawable.weather_storm, getResources().getColor(R.color.material_purple_a700)));
-
-        addSlide(AppIntro2Fragment.newInstance("Aunque no tengas Internet",
-                "fonts/Brandon_thin.otf",
-                "Recibiras boletines de emergencia en mensajes de texto por si no tienes Internet, " +
-                        "necesitas iniciar sesión para habilitarlo",
-                "fonts/Brandon_reg.otf",
-                R.drawable.weather_rain_day, getResources().getColor(R.color.colorAccent)));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro_fragment_one));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro_fragment_two));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro_fragment_three));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro_fragment_four));
 
 //        Ask for GPS permission on the second slide
 
-        setDepthAnimation();
+        setZoomAnimation();
         showStatusBar(false);
 
         showSkipButton(false);
